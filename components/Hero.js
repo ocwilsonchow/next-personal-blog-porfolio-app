affb07e91305c3d90f18d00ce1546f6ab96dd5ed
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-04-05 20:18:43 +0800

    updated index page text

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Button, Center, Flex, Text, VStack } from "@chakra-ui/react";
+import {
+  Badge,
+  Button,
+  Center,
+  Flex,
+  Tag,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import Image from "next/image";
 import Devdesk from "../images/devTransparent.png";
 import Link from "next/link";
@@ -9,18 +17,41 @@ const Hero = () => {
     <Flex p={2}>
       <Flex alignItems="center">
         <Flex flexDir="column" p={8}>
-          <Text fontWeight="bold" fontSize={{ base: "2xl", md: "4xl" }} textAlign="center">
+          <Text
+            fontWeight="bold"
+            fontSize={{ base: "2xl", md: "4xl" }}
+            textAlign="center"
+          >
             Launching Soon.
           </Text>
-          <Text fontWeight="thin" fontSize={{ base: "sm", md: "lg" }} textAlign="center">
-            This site will replace the old one.
+          <Text
+            fontWeight="thin"
+            fontSize={{ base: "sm", md: "lg" }}
+            textAlign="center"
+          >
+            This site is replacing the old one.
+          </Text>
+          <Text
+            fontWeight="thin"
+            fontSize={{ base: "sm", md: "lg" }}
+            textAlign="center"
+          >
+           Expected in mid-April 2022.
           </Text>
           <Link href="/about">
-            <Button mt={8} fontWeight="bold" size="lg" variant="outline" borderRadius="full">Discover More</Button>
+            <Button
+              mt={8}
+              fontWeight="bold"
+              size="lg"
+              variant="outline"
+              borderRadius="full"
+            >
+              Discover More
+            </Button>
           </Link>
         </Flex>
         <Flex>
-          <Image src={Devdesk} width="500px" height="500px"/>
+          <Image src={Devdesk} width="500px" height="500px" />
         </Flex>
       </Flex>{" "}
     </Flex>
